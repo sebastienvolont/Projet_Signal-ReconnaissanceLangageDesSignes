70320b7a7f94c6024d190f3480270c574a4fc574
cyrilGrandjean <64021617+cyrilGrandjean@users.noreply.github.com>
2021-12-08 23:24:02 +0100

    new model

--- train.py
+++ train.py
@@ -1,6 +1,12 @@
+from pprint import pprint
+from string import ascii_lowercase
+
+import numpy as np
+
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import Sequential, layers
+from tensorflow.keras.preprocessing.image import img_to_array, load_img
 
 batch_size = 32
 img_height = 32
@@ -34,13 +40,13 @@ val_ds = val_ds.cache().prefetch(buffer_size=tf.data.AUTOTUNE)
 # Define the model
 model = Sequential([
     layers.Rescaling(1 / 255),
-    layers.Conv2D(32, 3, activation='relu', input_shape=(32, 32, 3)),
-    layers.MaxPooling2D(),
-    layers.Conv2D(64, 3, activation='relu'),
-    layers.MaxPooling2D(),
-    layers.Conv2D(64, 3, activation='relu'),
+    layers.Conv2D(16, (3, 3), strides=(1, 1), padding="valid", activation="relu", input_shape=(32, 32, 3)),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(16, 3, activation="relu"),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(32, 3, activation="relu"),
     layers.Flatten(),
-    layers.Dense(64, activation='relu'),
+    layers.Dense(32, activation="relu"),
     layers.Dense(26)
 ])
 
@@ -58,12 +64,23 @@ model.compile(
 model.fit(
     train_ds,
     validation_data=val_ds,
-    epochs=10,
+    epochs=3,
     batch_size=batch_size,
 )
 # Save the trained model
 model.save("model")
 
-
 loss, acc = model.evaluate(val_ds, verbose=2)
 print(f'Loss: {loss:.2%} Accuracy: {acc:.2%}')
+
+
+img = np.array([img_to_array(load_img('test2/b.jpg', color_mode='grayscale'))])
+print('Prediction for image b:', ascii_lowercase[np.argmax(model.predict(img))])
+# pprint(model.predict(img))
+
+img = np.array([img_to_array(load_img('test2/c.jpg', color_mode='grayscale'))])
+print('Prediction for image c:', ascii_lowercase[np.argmax(model.predict(img))])
+# pprint(model.predict(img))
+
+img = np.array([img_to_array(load_img('test2/k.jpg', color_mode='grayscale'))])
+print('Prediction for image k:', ascii_lowercase[np.argmax(model.predict(img))])
